287a4f914cee6f3ebb6237a92c4e1ee33ae6d25a
J0kk3 <joakim_88@msn.com>
2022-06-30 00:35:20 +0200

    [ADD] firebase init, hooks & auth context

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOr5VJtA98YX9wdL58T7ZE7VAXVIlNL_k",
+    authDomain: "project-managment-2dc16.firebaseapp.com",
+    projectId: "project-managment-2dc16",
+    storageBucket: "project-managment-2dc16.appspot.com",
+    messagingSenderId: "45215087731",
+    appId: "1:45215087731:web:611d23168f3f4520f07e06"
+};
+
+//Initialize firebase
+firebase.initializeApp( firebaseConfig );
+//Initialize services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//Timestamp
+const Timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, Timestamp };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { authContextProvider } from './context/authContext';
 //Styles
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <authContextProvider>
+      <App />
+    </authContextProvider>
   </React.StrictMode>,
   document.getElementById( 'root' )
 );
