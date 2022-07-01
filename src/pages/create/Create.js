308bde46a80eab9f226a20e9dfca639508fc6489
J0kk3 <joakim_88@msn.com>
2022-07-01 09:03:29 +0200

    [BUG] missing closing curly-bracket

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -99,65 +99,65 @@ export default function Create ()
     }
   }
 
-    return (
-      <div className="create-form">
-        <h2 className='page-title' >Create a new project</h2>
-        <form onSubmit={ handleSubmit }>
-          <label>
-            <span>Project Name:</span>
-            <input
-              required
-              type="text"
-              onChange={ ( e ) => setName( e.target.value ) }
-              value={ name }
-            />
-          </label>
-          <label>
-            <span>Project Details:</span>
-            <textarea
-              required
-              type="text"
-              onChange={ ( e ) => setDetails( e.target.value ) }
-              value={ details }
-            ></textarea>
-          </label>
-          <label>
-            <span>Set Due Date:</span>
-            <input
-              required
-              type="date"
-              onChange={ ( e ) => setDueDate( e.target.value ) }
-              value={ dueDate }
-            />
-          </label>
-          <label>
-            <span>Priority (max 5):</span>
-            <input
-              type="number"
-              max="10"
-              onChange={ ( e ) => setPriority( e.target.value ) }
-              value={ priority }
-            />
-          </label>
-          <label>
-            <span>Project Category:</span>
-            <Select
-              onChange={ ( option ) => setCategory( option ) }
-              options={ categories }
-            />
-          </label>
-          <label>
-            <span>Assign To:</span>
-            <Select
-              onChange={ ( option ) => setAssignedUsers( option ) }
-              options={ users }
-              isMulti
-            />
-          </label>
-          <button className='btn'>Add Project</button>
+  return (
+    <div className="create-form">
+      <h2 className='page-title' >Create a new project</h2>
+      <form onSubmit={ handleSubmit }>
+        <label>
+          <span>Project Name:</span>
+          <input
+            required
+            type="text"
+            onChange={ ( e ) => setName( e.target.value ) }
+            value={ name }
+          />
+        </label>
+        <label>
+          <span>Project Details:</span>
+          <textarea
+            required
+            type="text"
+            onChange={ ( e ) => setDetails( e.target.value ) }
+            value={ details }
+          ></textarea>
+        </label>
+        <label>
+          <span>Set Due Date:</span>
+          <input
+            required
+            type="date"
+            onChange={ ( e ) => setDueDate( e.target.value ) }
+            value={ dueDate }
+          />
+        </label>
+        <label>
+          <span>Priority (max 5):</span>
+          <input
+            type="number"
+            max="10"
+            onChange={ ( e ) => setPriority( e.target.value ) }
+            value={ priority }
+          />
+        </label>
+        <label>
+          <span>Project Category:</span>
+          <Select
+            onChange={ ( option ) => setCategory( option ) }
+            options={ categories }
+          />
+        </label>
+        <label>
+          <span>Assign To:</span>
+          <Select
+            onChange={ ( option ) => setAssignedUsers( option ) }
+            options={ users }
+            isMulti
+          />
+        </label>
+        <button className='btn'>Add Project</button>
 
-          { formError && <p className='error'>{ formError }</p> }
-        </form>
-      </div>
-    );
-  }
+        { formError && <p className='error'>{ formError }</p> }
+      </form>
+    </div>
+  );
+}
